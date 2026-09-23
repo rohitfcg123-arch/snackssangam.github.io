ae9e9a24b7f516ceeaefd1228933fbbd7be93140
rohitfcg123-arch <rohit.fcg123@gmail.com>
2026-09-23 14:41:57 +0530

    Add files via upload

--- config/portal-config.js
+++ config/portal-config.js
@@ -1,1 +1,7 @@
-
+window.CMA_CONFIG={
+  ADMIN_EMAIL:"rohit.fcg123@gmail.com",
+  PAYMENT_PAGE:"payment.html",
+  HOME_PAGE:"index.html",
+  FIREBASE_CONFIG:{"apiKey": "AIzaSyCDQRVXCxHZfy3cLUJg2jG3kUTuBABHMbc", "authDomain": "cma-mcq-portal-cf33f.firebaseapp.com", "projectId": "cma-mcq-portal-cf33f", "storageBucket": "cma-mcq-portal-cf33f.firebasestorage.app", "messagingSenderId": "208738737302", "appId": "1:208738737302:web:3754a55033f67a8ffef6b7", "measurementId": "G-TXD74QQE8F"},
+  DEFAULT_ACCESS:{freeAccessMode:"fixedPool",freePoolSize:20,freeAttemptLimit:5,siteWideFree:false}
+};
